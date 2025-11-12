3ec9bdd623b7a6c0a327f31eb0279e32269010eb
Jeanie <rowr111@users.noreply.github.com>
2025-11-12 21:34:23 +0900

    fix requirements location

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 
 export const REQUIRED_TOOLS_BAO = '0.1.2';
 export const BAO_VENV_DIR = '.venv';
-export const REQUIREMENTS_FILE = 'requirements.txt';
+export const REQUIREMENTS_FILE = 'tools-bao/requirements.txt';
 export const STATE_KEY_REQ_HASH_MAP = 'bao.toolsBao.reqHashByRoot';
